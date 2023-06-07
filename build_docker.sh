b7ba32eea65a3265c1ac14a6b37aa86a9c1d5db2
XiaoBeing <94702632+Mr-jiangrj@users.noreply.github.com>
2023-06-07 22:28:44 +0800

    Delete build_docker.sh

--- build_docker.sh
+++
@@ -1,7 +0,0 @@
-registry="registry.cn-hangzhou.aliyuncs.com"
-image="registry.cn-hangzhou.aliyuncs.com/jiangrj/sd-webui"
-docker build -t $image:latest .
-docker tag $image:latest $image:$(date +'%Y-%m-%d')
-docker login --username=1326890495@qq.com $registry
-docker push $image:latest
-docker push $image:$(date +'%Y-%m-%d')
