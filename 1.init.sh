8ede56cc254dfc2525d86d5036a3fb280e6b13ad
XiaoBeing <94702632+Mr-jiangrj@users.noreply.github.com>
2023-06-07 22:21:26 +0800

    Update 1.init.sh

--- 1.init.sh
+++ 1.init.sh
@@ -1,12 +1,7 @@
-su - root
-
 docker images -q | xargs docker rmi -f
-rm -rf /var/lib/docker/overlay2/*
-rm -rf /opt/hostedtoolcache/*
-swapoff -a
-rm -rf /mnt/*
-chown runner:runner -R /mnt
-
-su - runner
-
+sudo rm -rf /var/lib/docker/overlay2/*
+sudo rm -rf /opt/hostedtoolcache/*
+sudo swapoff -a
+sudo rm -rf /mnt/*
+sudo chown runner:runner -R /mnt
 bash <(wget -qO- https://raw.githubusercontent.com/AUTOMATIC1111/stable-diffusion-webui/master/webui.sh) --skip-torch-cuda-test
