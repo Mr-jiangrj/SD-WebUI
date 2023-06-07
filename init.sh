b44557a170b497437816f3bf75588fad34f48afd
XiaoBeing <94702632+Mr-jiangrj@users.noreply.github.com>
2023-06-07 22:28:25 +0800

    Delete init.sh

--- init.sh
+++
@@ -1,7 +0,0 @@
-docker images -q | xargs docker rmi -f
-sudo rm -rf /var/lib/docker/overlay2/*
-sudo rm -rf /opt/hostedtoolcache/*
-sudo swapoff -a
-sudo rm -rf /mnt/*
-sudo chown runner:runner -R /mnt
-bash <(wget -qO- https://raw.githubusercontent.com/AUTOMATIC1111/stable-diffusion-webui/master/webui.sh) --skip-torch-cuda-test
